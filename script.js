99949bbf54a8754778774175fd8e67fea8617ed5
Elvys <elvysd@gmail.com>
2021-07-14 11:14:22 -0700

    Initial changes

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+// Player Factory
 
+const Player = (name, turn) => {
+    const getName = () => name;
+    const getTurn = () => turn;
+    const win = () => {
+        //
+    }
+    const myTurn = ""
+
+    return {getName, getTurn}
+};
+
+const x = Player("X", 'yes');
+const o = Player("O", 'no');
+
+// GameBoard Module
+
+const gameBoard = (() => {
+
+    const 
+
+})();
+
+// Cotroller Module
+
+myTurn = () => {
+
+}
